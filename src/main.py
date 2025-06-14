25149424fa81b9b97afbabe3684cc889a90c0814
cole <f8cole@gmail.com>
2025-06-14 19:32:27 -0400

    add check

--- src/main.py
+++ src/main.py
@@ -1,17 +1,26 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import AzureBlobStorageFileLoader
 from langchain.schema import Document
-import asyncio
-from azure.storage.blob import BlobClient
+from azure.storage.blob import BlobClient, BlobServiceClient
 import tempfile
 import os
 
 
 
+def get_new_Azure_blobs(connection_string , container_name) -> list[Document]:
+    documents = []    
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
+    for blob in container_client.list_blobs():
+        metadata = blob.metadata or {}
+        if 'IsIndexed' not in metadata:
+            documents.extend(load_Azure_Blob(blob.name, container_name, connection_string))
+            print("added " + blob.name)
+    return documents
 
 
-def ingest_Azure_Blob(blob_name : str, container_name : str, conn_str : str, additional_metadata : dict[str,str] | None = None ) -> Document:
-    blob_client = BlobClient.from_blob_url(file_path)
+def load_Azure_Blob(blob_name : str, container_name : str, conn_str : str, additional_metadata : dict[str,str] | None = None ) -> Document:
+    blob_client = BlobClient.from_connection_string(conn_str, container_name, blob_name)
     blob_props = blob_client.get_blob_properties()
     blob_metadata = dict(blob_props.metadata) if blob_props.metadata else {}
 
@@ -20,25 +29,25 @@ def ingest_Azure_Blob(blob_name : str, container_name : str, conn_str : str, add
         tmp_file.write(download_stream.readall())
         tmp_file_path = tmp_file.name
 
-    doc = AzureBlobStorageFileLoader(conn_str=conn_str, container=container_name, conn_str=conn_str).load()
-    
+    docs = AzureBlobStorageFileLoader(conn_str=conn_str, container=container_name, blob_name=blob_name).load()
     for k, v in blob_metadata.items():
-        doc.metadata[k] = v
-    for k,v in additional_metadata:
-        doc.metadata[k] = v
-
-    doc.metadata['blob_name'] = blob_client.blob_name
-    doc.metadata['container'] = blob_client.container_name
-    doc.metadata['size'] = blob_props.size
+        for item in docs:
+            item.metadata[k] = v
+    if additional_metadata is not None:
+        for k,v in additional_metadata:
+            for item in docs:
+                item.metadata[k] = v
+    for item in docs:
+        item.metadata['blob_name'] = blob_client.blob_name
+        item.metadata['container'] = blob_client.container_name
+        item.metadata['size'] = blob_props.size
     os.remove(tmp_file_path)
-    return doc
+    return docs
 
-
+def chunk_document(doc : Document) -> list[Document]:
     
 
-        
-            
-    
 
-        
-asyncio.run(ingest_PDF("C:\\Users\\f8col\\OneDrive\\Desktop\\Projects\\docPipe\\HierarchicalRAG.pdf"))
+
+conn_str = "BlobEndpoint=https://biceptest.blob.core.windows.net/;QueueEndpoint=https://biceptest.queue.core.windows.net/;FileEndpoint=https://biceptest.file.core.windows.net/;TableEndpoint=https://biceptest.table.core.windows.net/;SharedAccessSignature=sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-06-16T05:08:57Z&st=2025-06-14T21:08:57Z&spr=https&sig=0%2BdqfHMXKUpkRa7wga%2FV4Oy7W4UEyv%2BWQY2mbXLaoLY%3D"
+container_name = 'original'
